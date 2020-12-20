a33c467d2796c88c403179876a181b7691099ce9
unknown <linkinmjs@gmail.com>
2020-12-20 11:47:57 -0300

    Creando un listado de noticias

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Fragment, useState, useEffect} from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 
 function App() {
 
   // definir la categoría y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
 
   useEffect(()=>{
     const consultarAPI = async() => {
@@ -16,20 +18,26 @@ function App() {
         const noticias = await respuesta.json();
 
         console.log(noticias);
+
+        guardarNoticias(noticias.articles)
     }
     consultarAPI()
   }, [categoria])
 
   return (
-    <>
+    <Fragment>
       <Header titulo="Buscador de Noticias"/>
 
       <div className="container white">
         <Formulario
           guardarCategoria={guardarCategoria}
         />
+
+        <ListadoNoticias 
+          noticias={noticias}
+        />
       </div>
-    </>
+    </Fragment>
   );
 }
 
